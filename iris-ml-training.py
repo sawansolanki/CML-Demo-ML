2f6a3fc2a8391d3cf4f8c25fa1afd802798f8235
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2023-09-15 11:42:15 +0530

    Update iris-ml-training.py

--- iris-ml-training.py
+++ iris-ml-training.py
@@ -83,7 +83,7 @@ X.shape
 # In[12]:
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=42)
 
 model = DecisionTreeClassifier()
 
